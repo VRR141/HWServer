1d53cc39c2215239cce9c8923951626e9f27eed2
Vladislav <v.r.ryazantsev@gmail.com>
2022-09-15 20:00:04 +0300

    init

---
+++ src/main/java/Server.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private static int port = 8089;
+
+
+    public static void main(String[] args) {
+        System.out.println("SERVER STARTED");
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    System.out.printf("New connection accepted. Port %d%n", clientSocket.getPort());
+                    String name = in.readLine();
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
